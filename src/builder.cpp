615491063f38af0ecab6a666fc0b86ab20c776dc
Encounter005 <1624221574@qq.com>
2024-07-13 00:26:44 +0800

    ADD: test_func

--- src/builder.cpp
+++ src/builder.cpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <memory>
 
 // Product
 class Computer {
@@ -10,56 +11,63 @@ public:
     void setMemory( const std::string &memory ) { memory_ = memory; }
     void setStorage( const std::string &storage ) { storage_ = storage; }
     void print() const {
-        std::cout << "CPU:     " << cpu_     << std::endl;
-        std::cout << "Memory:  " << memory_  << std::endl;
+        std::cout << "CPU:     " << cpu_ << std::endl;
+        std::cout << "Memory:  " << memory_ << std::endl;
         std::cout << "Storage: " << storage_ << std::endl;
     }
 
 private:
-    std::string cpu_    ;
-    std::string memory_ ;
+    std::string cpu_;
+    std::string memory_;
     std::string storage_;
 };
 
 // Builder interface
 class ComputerBuilder {
 public:
+    virtual ~ComputerBuilder()                       = default;
     virtual void buildCPU( const std::string & )     = 0;
     virtual void buildMemory( const std::string & )  = 0;
     virtual void buildStorage( const std::string & ) = 0;
-    virtual Computer getResult()                     = 0;
+    virtual std::shared_ptr<Computer> getResult()    = 0;
 };
 
 // Concrete Builder
 class DesktopComputerBuilder : public ComputerBuilder {
 public:
-    DesktopComputerBuilder() { computer_ = Computer(); }
+    DesktopComputerBuilder() : computer_( std::make_shared<Computer>() ) {}
 
     void buildCPU( const std::string &cpu ) override {
-        computer_.setCPU( cpu );
+        computer_->setCPU( cpu );
     }
     void buildMemory( const std::string &memory ) override {
-        computer_.setMemory( memory );
+        computer_->setMemory( memory );
     }
     void buildStorage( const std::string &storage ) override {
-        computer_.setStorage( storage );
+        computer_->setStorage( storage );
     }
 
-    Computer getResult() override { return computer_; }
+    std::shared_ptr<Computer> getResult() override { return computer_; }
 
 private:
-    Computer computer_;
+    std::shared_ptr<Computer> computer_;
 };
 
 // Director
 
 class ComputerAssembler {
 public:
-    Computer assembleComputer(ComputerBuilder& builder) {
-        builder.buildCPU("Inter i7");
-        builder.buildStorage("980 PRO 1TB SSD");
-        builder.buildMemory("16GB");
+    std::shared_ptr<Computer> assembleComputer( ComputerBuilder &builder ) {
+        builder.buildCPU( "Inter i7" );
+        builder.buildStorage( "980 PRO 1TB SSD" );
+        builder.buildMemory( "16GB" );
         return builder.getResult();
     }
 };
 
+void test_func() {
+    DesktopComputerBuilder builder;
+    ComputerAssembler assembler;
+    auto computer = assembler.assembleComputer( builder );
+    computer->print();
+}
